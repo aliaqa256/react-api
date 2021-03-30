57b17b82775127bc8f2613fb32e9c579c7d81f3b
aliaqa <alilotfi256@gmail.com>
2021-03-30 12:39:28 +0430

    akharin ba ali

--- src/Components/Admin/Blog/BlogList.jsx
+++ src/Components/Admin/Blog/BlogList.jsx
@@ -1,12 +1,43 @@
-import React, { Fragment } from 'react';
+import React, { Fragment,useEffect,useState } from 'react';
 import BlogListItem from './BlogListItem';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const url="http://localhost:8000"
 
 
 
+const BlogList = () =>
+{
+    
+    const [ getallblog, setallblog ] = useState( [] )
 
-const BlogList = () => {
+    const getallblogapi=() =>
+    {
+        
+        axios.get( url + "/api/blog" ).then( ( {data} ) =>
+        {
+            setallblog( data )
+            
+            
+        } )
+            .catch( ex =>
+            {
+                
+                console.log( ex );
+            } );
 
+
+    }
+    
+    
+   
+    useEffect( () =>
+    {
+        getallblogapi()
+        console.log(getallblog);
+        
+    }, [] );
 
     return (
         <Fragment>
@@ -20,9 +51,13 @@ const BlogList = () => {
 
                 
        </Link>
-
-
-            <BlogListItem />
+   
+     
+       
+           
+                <BlogListItem />
+    
+          
 
         </Fragment>
     );
--- src/Components/Admin/Blog/BlogListItem.jsx
+++ src/Components/Admin/Blog/BlogListItem.jsx
@@ -1,23 +1,42 @@
 import React from 'react';
 
-const BlogListItem = () => {
+const BlogListItem = (props) => {
     return (
 
+      
+            
         <div className="AdminBlogList text-truncate row ">
-            <div className="col  BlogListItem text-truncate">Blog.Title</div>
-            <i className="verticalLine"></i>
-            <div className="col BlogListItem text-truncate">Blog.User</div>
-            <i className="verticalLine"></i>
-            <div className="col BlogListItem text-truncate">Blog.Date</div>
-            <div className="row BlogListIcon">
-                <div className="col BlogListItem">
-                    <i className="fas fa-edit EditIcon"></i>
+            {console.log( props )}
+
+                    <div className="col  BlogListItem text-truncate">blog.title </div>
+                    <i className="verticalLine"></i>
+                    <div className="col BlogListItem text-truncate">Blog.User</div>
+                    <i className="verticalLine"></i>
+                    <div className="col BlogListItem text-truncate">Blog.Date</div>
+                    <div className="row BlogListIcon">
+                        <div className="col BlogListItem">
+                            <i className="fas fa-edit EditIcon"></i>
+                        </div>
+                        <div className="col BlogListItem">
+                            <i className="fas fa-trash-alt TrashIcon"></i>
+                        </div>
+                    </div>
                 </div>
-                <div className="col BlogListItem">
-                    <i className="fas fa-trash-alt TrashIcon"></i>
-                </div>
-            </div>
-        </div>
+
+    
+       
+        
+    
+            
+
+   
+        
+    
+
+    
+    
+       
+      
     );
 }
 
